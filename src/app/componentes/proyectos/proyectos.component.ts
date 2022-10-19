fdd296a44a731ea00719e58c2585beb35b003e10
Gustavo <villegasgustavoopel@hotmail.com>
2022-10-19 07:56:22 -0300

    agregando directivas a componentes

--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private datosPortafoliofrontend:PortfolioService) { }
 
+  miPorfolio:any
   ngOnInit(): void {
+    this.datosPortafoliofrontend.obtenerDatos().subscribe(data => {
+      console.log(data.proyectos)
+      this.miPorfolio=data.proyectos;
+      
+    })
   }
-
 }
